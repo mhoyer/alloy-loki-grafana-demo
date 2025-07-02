e39e8c8a6b2c5ba4fa5dde0ac5ec070d1723beb3
Marcel Hoyer <m.hoyer@cid.com>
2025-07-02 13:28:37 +0000

    wrap Loki helm chart into its own helm chart

--- loki/install.sh
+++ loki/install.sh
@@ -1,5 +1,3 @@
 #!/bin/sh
 
-kubectl create namespace platform
-
-helm upgrade  -i -n platform loki grafana/loki --version 6.29.0 -f ./values.yaml
+helm upgrade -i -n platform --dependency-update loki . -f ./values.yaml
